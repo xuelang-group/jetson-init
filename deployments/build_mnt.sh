0b58cf0ac6f2ca2be198cf84485aed6f0d69e47d
majik <me@yamajik.com>
2020-04-07 13:23:40 +0800

    link /mnt/suanpan & /mnt/sdcard

--- deployments/build_mnt.sh
+++ deployments/build_mnt.sh
@@ -1,5 +1,8 @@
 mkdir -p /mnt/ssd
 mount /dev/sda1 /mnt/ssd
+ln -s /mnt/ssd/suanpan /mnt/suanpan
+ln -s /mnt/ssd/sdcard /mnt/sdcard
+
 if grep -q sda1 /etc/fstab ; then
 echo find sda1 exist in fstab, do nothing
 else
