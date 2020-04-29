23869763a02eb26c7b7c3dbf5d30fa2fd3b4b59f
majik <me@yamajik.com>
2020-04-29 16:43:15 +0800

    fix

--- deployments/build_mnt.sh
+++ deployments/build_mnt.sh
@@ -2,9 +2,10 @@ ssd_path=/dev/sda
 mkfs.ext4 ${ssd_path}
 mkdir -p /mnt/ssd
 mount  ${ssd_path} /mnt/ssd
-mkdir -p /mnt/ssd/sdcard /mnt/ssd/suanpan
+mkdir -p /mnt/ssd/global/data /mnt/ssd/global/configs
+mkdir -p /mnt/ssd/suanpan
 ln -s /mnt/ssd/suanpan /mnt/suanpan
-ln -s /mnt/ssd/sdcard /mnt/sdcard
+ln -s /mnt/ssd/global /mnt/global
 
 if grep -q sda1 /etc/fstab ; then
 echo find sda1 exist in fstab, do nothing
